aca35ee123d11bb780033a854b59bdd0615edd1c
Anmol Kumar Singh <42077305+AnmolSingh-01@users.noreply.github.com>
2021-12-30 21:25:48 +0530

    Update combinationFormula.java

--- combinationFormula.java
+++ combinationFormula.java
@@ -9,8 +9,14 @@ public class combinationFormula {
         den = fact(r)*fact(n-r);
         return num/den;
     }
+    // Recursive Approach
+   static int NCR(int n , int r){
+        if(n==r || r==0) return 1;
+        return NCR(n-1,r-1) + NCR(n-1,r);
+   }
+   //////////////////////
 
     public static void main(String[] args) {
-        System.out.println("The value corresponding to given n and r is "+nCr(5,1));
+        System.out.println("The value corresponding to given n and r is "+nCr(5,1));  ///  we can use here NCR also
     }
 }
